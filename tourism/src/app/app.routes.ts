0bdaa909caecf9af84166259a8afa4964eeba650
Cristian Fierro <ccamilofierro@gmail.com>
2024-06-15 14:36:45 -0500

    Se crea un modulo de como llegar

--- tourism/src/app/app.routes.ts
+++ tourism/src/app/app.routes.ts
@@ -3,12 +3,17 @@ import { HomeComponent } from './components/home/home.component';
 import { HotelComponent } from './components/hotel/hotel.component';
 import { AgenciasComponent } from './components/agencias/agencias.component';
 import { EstadisticasComponent } from './components/estadisticas/estadisticas.component';
+import { HowToGetComponent } from './components/how-to-get/how-to-get.component';
 
 export const routes: Routes = [
 
     {
         path:'home',
         component: HomeComponent
+    },
+    {
+        path:'como-llegar',
+        component: HowToGetComponent
     },
     {
         path:'hotel',component:HotelComponent
---
+++ tourism/src/app/components/how-to-get/how-to-get.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-how-to-get',
+  standalone: true,
+  imports: [],
+  templateUrl: './how-to-get.component.html',
+  styleUrl: './how-to-get.component.css'
+})
+export class HowToGetComponent {
+
+}
